f743166d720df214851c58f85c678d44285e3a4a
Jagan Kumar G <jagan@hurreytech.com>
2019-12-21 13:09:49 +0530

    some more correction

--- adv.go
+++ adv.go
@@ -95,7 +95,7 @@ func (a *Advertisement) unmarshall(b []byte) error {
 		}
 		l, t := b[0], b[1]
 		if len(b) < int(1+l) {
-			fmt.Printf("Invalid advertise data found, size=%d, l = %d, t = %d",
+			fmt.Printf("Invalid advertise data found, size=%d, l = %d, t = %d\n",
 				len(b), l, t)
 			return errors.New("invalid advertise data")
 		}
